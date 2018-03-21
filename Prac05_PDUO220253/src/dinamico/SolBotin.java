74b9e593831b760371115944bfca7a226adc7c7b
Cristto <cristotrader@gmail.com>
2018-03-21 17:00:46 +0100

    cambio en clase

--- Prac05_PDUO220253/src/dinamico/SolBotin.java
+++ Prac05_PDUO220253/src/dinamico/SolBotin.java
@@ -5,13 +5,29 @@ import inicio.LeerBotin;
 
 public class SolBotin {
 
-    Joya[][] botin;
 
 
-    public SolBotin(){
+    public void solDinamico(Joya[] a, int fila, int col){
 
-        botin = new Joya[LeerBotin.getN()][LeerBotin.getK()];
+        int [][] botin = new int[fila+1][col+1];
 
 
+        for (int i = 0; i < fila+1; i++) {
+            for (int j = 0; j < col+1; j++) {
+                if(i == 0)
+                    botin[0][j] = 0;
+                
+            }
+        }
+
+    }
+
+    public void imprimir(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
